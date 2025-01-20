cc292210c867d69de2c884905857ebc68bba74bc
Peter <youngeunpc@yahoo.com>
2025-01-20 19:07:29 -0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <main>
+            <div className="flex items-center justify-center">
+                <img src="/images/chef-claude-icon.png" />
+                <h1>Chef Claude</h1>
+            </div>
+        </main>
+    )
+}
