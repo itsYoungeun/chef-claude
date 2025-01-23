07294e27adcb5b39586e243b4c1fdc1ff0a27a27
Peter <youngeunpc@yahoo.com>
2025-01-22 22:34:56 -0300

    Applying state to update ingredients list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,7 @@
+import React from 'react';
+
 export default function Main() {
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    const [ingredients, setIngredients] = React.useState([]);
 
     const ingredientsListItems = ingredients.map((ingredient) => {
         return <li key={ingredient}>{ingredient}</li>
@@ -7,11 +9,9 @@ export default function Main() {
 
     function handleSubmit(event) {
         event.preventDefault()
-        console.log("Form submitted!")
         const formData = new FormData(event.currentTarget)
         const newIngredient = formData.get("ingredient")
-        ingredients.push(newIngredient)
-        console.log(ingredients)
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
 
     return (
